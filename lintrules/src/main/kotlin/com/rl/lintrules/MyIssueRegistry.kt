6d58c510f1cbe1e04f73614ec46c91daeca11170
= <=>
2019-03-14 10:06:37 +0100

    Static import

--- lintrules/src/main/kotlin/com/rl/lintrules/MyIssueRegistry.kt
+++ lintrules/src/main/kotlin/com/rl/lintrules/MyIssueRegistry.kt
@@ -1,11 +1,12 @@
 package com.rl.lintrules
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 
 class MyIssueRegistry : IssueRegistry() {
     override val issues: List<Issue>
         get() = listOf(ISSUE_USER_INTERFACE_INHERITANCE)
 
-    override val api: Int = com.android.tools.lint.detector.api.CURRENT_API
+    override val api: Int = CURRENT_API
 }
