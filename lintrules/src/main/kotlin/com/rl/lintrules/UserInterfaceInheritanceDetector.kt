862f4411a01b4cc54df2b97b216389237a588acf
= <=>
2019-03-14 10:55:00 +0100

    Named parameters for creating issue

--- lintrules/src/main/kotlin/com/rl/lintrules/UserInterfaceInheritanceDetector.kt
+++ lintrules/src/main/kotlin/com/rl/lintrules/UserInterfaceInheritanceDetector.kt
@@ -5,26 +5,26 @@ import org.jetbrains.uast.UClass
 import org.jetbrains.uast.UElement
 import java.util.*
 
-val ISSUE_USER_INTERFACE_INHERITANCE = Issue.create("UserInterfaceInheritance",
-        "Bad inheritance",
-    UserInterfaceInheritanceDetector.MESSAGE,
-        Category.CORRECTNESS,
-        5,
-        Severity.ERROR,
-        Implementation(
-            UserInterfaceInheritanceDetector::class.java,
-                EnumSet.of(Scope.JAVA_FILE)))
-
+val ISSUE_USER_INTERFACE_INHERITANCE = Issue.create(
+    id = "UserInterfaceInheritance",
+    briefDescription = "Bad inheritance",
+    explanation = UserInterfaceInheritanceDetector.MESSAGE,
+    category = Category.CORRECTNESS,
+    priority = 5,
+    severity = Severity.ERROR,
+    implementation = Implementation(
+        UserInterfaceInheritanceDetector::class.java,
+        EnumSet.of(Scope.JAVA_FILE)))
 
 class UserInterfaceInheritanceDetector : Detector(), Detector.UastScanner {
 
     companion object {
         const val MESSAGE = "Let activities and fragments inherit directly from Android's Activity or Fragment"
         private val UI_CLASSES =  listOf(
-                "android.support.v7.app.AppCompatActivity",
-                "androidx.appcompat.app.AppCompatActivity",
-                "android.support.v4.app.Fragment",
-                "androidx.fragment.app")
+            "android.support.v7.app.AppCompatActivity",
+            "androidx.appcompat.app.AppCompatActivity",
+            "android.support.v4.app.Fragment",
+            "androidx.fragment.app")
     }
 
     override fun getApplicableUastTypes(): List<Class<out UElement>>? = listOf(UClass::class.java)
@@ -40,7 +40,7 @@ class UserInterfaceInheritanceDetector : Detector(), Detector.UastScanner {
             if (context.evaluator.getQualifiedName(superClass) !in UI_CLASSES) {
                 context.report(
                     ISSUE_USER_INTERFACE_INHERITANCE, declaration,
-                        context.getNameLocation(declaration),
+                    context.getNameLocation(declaration),
                     MESSAGE
                 )
             }
