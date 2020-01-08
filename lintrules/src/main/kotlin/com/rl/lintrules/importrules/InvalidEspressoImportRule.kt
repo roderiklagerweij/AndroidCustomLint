03beca489fa940937f77a51a29b8f05355257560
Roderik Lagerweij <roderiklagerweij@MacBook-Pro-van-Roderik-2.local>
2020-01-08 14:39:27 +0100

    Fixed tests

--- lintrules/src/main/kotlin/com/rl/lintrules/importrules/InvalidEspressoImportRule.kt
+++ lintrules/src/main/kotlin/com/rl/lintrules/importrules/InvalidEspressoImportRule.kt
@@ -13,7 +13,5 @@ class InvalidEspressoImportRule : InvalidImportRule {
         return true
     }
 
-    override fun getMessage(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getMessage() = "Please don't use any Espresso code in your test classes"
 }
